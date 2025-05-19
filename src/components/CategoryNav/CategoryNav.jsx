39690284fc1b89f5760461cce7808b882961125f
SK Rizwan <skrizwan1219@gmail.com>
2025-05-19 15:03:25 +0530

    fix: UI improvements - Adjusted sidebar spacing and icons, fixed category navigation, improved responsive layout

--- src/components/CategoryNav/CategoryNav.jsx
+++ src/components/CategoryNav/CategoryNav.jsx
@@ -1,22 +1,25 @@
 import React from 'react';
 import './CategoryNav.css';
+import { AiFillHome } from 'react-icons/ai';
+import { FaMusic, FaGamepad, FaCode, FaReact, FaCss3Alt, FaLaptopCode, FaPalette, FaNewspaper, FaFire, FaChartLine, FaFutbol, FaGlobe } from 'react-icons/fa';
+import { SiTailwindcss } from 'react-icons/si';
 
 const categories = [
-  "All",
-  "Music",
-  "Gaming",
-  "JavaScript",
-  "React",
-  "CSS",
-  "Tailwind",
-  "Coding",
-  "Web Development",
-  "UI Design",
-  "Latest",
-  "Popular",
-  "Trending",
-  "Sports",
-  "News"
+  { name: "All", icon: <AiFillHome size={18} /> },
+  { name: "Music", icon: <FaMusic size={18} /> },
+  { name: "Gaming", icon: <FaGamepad size={18} /> },
+  { name: "JavaScript", icon: <FaCode size={18} /> },
+  { name: "React", icon: <FaReact size={18} /> },
+  { name: "CSS", icon: <FaCss3Alt size={18} /> },
+  { name: "Tailwind", icon: <SiTailwindcss size={18} /> },
+  { name: "Coding", icon: <FaLaptopCode size={18} /> },
+  { name: "Web Development", icon: <FaCode size={18} /> },
+  { name: "UI Design", icon: <FaPalette size={18} /> },
+  { name: "Latest", icon: <FaNewspaper size={18} /> },
+  { name: "Popular", icon: <FaFire size={18} /> },
+  { name: "Trending", icon: <FaChartLine size={18} /> },
+  { name: "Sports", icon: <FaFutbol size={18} /> },
+  { name: "News", icon: <FaGlobe size={18} /> }
 ];
 
 const CategoryNav = () => {
@@ -27,11 +30,12 @@ const CategoryNav = () => {
       <div className="category-scroll no-scrollbar">
         {categories.map((category) => (
           <button
-            key={category}
-            className={`category-btn ${activeCategory === category ? 'active' : ''}`}
-            onClick={() => setActiveCategory(category)}
+            key={category.name}
+            className={`category-btn ${activeCategory === category.name ? 'active' : ''}`}
+            onClick={() => setActiveCategory(category.name)}
           >
-            {category}
+            <span className="category-icon">{category.icon}</span>
+            <span className="category-name">{category.name}</span>
           </button>
         ))}
       </div>
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,42 +1,40 @@
 import React from 'react';
 import './Navbar.css';
-import { Search, Plus, Bell, Mic } from 'lucide-react';
+import { Menu, Search, Plus, Bell, Mic, Home, Music, User } from 'lucide-react';
 import logo from '../../assets/img/logo.png';
-import profile_icon from '../../assets/img/rizz.jpeg'; 
+import profile_icon from '../../assets/img/rizz.jpeg';
 
 export const Navbar = ({ toggleSidebar }) => {
   return (
     <nav className='flex-div'>
       <div className='nav-left flex-div'>
-        <div className="hamburger-menu" onClick={toggleSidebar}>
-          <div className="bar"></div>
-          <div className="bar"></div>
-          <div className="bar"></div>
-        </div>
+        {/* Hamburger menu icon */}
+        <Menu className="menu-icon" size={28} onClick={toggleSidebar} style={{ cursor: 'pointer' }} />
         <img className='logo' src={logo} alt="Logo" />
       </div>
 
       <div className='nav-middle flex-div'>
-        <div className='search-box flex-div'> 
+        <div className='search-box flex-div'>
           <input type="text" placeholder='Search' className='search-bar' />
           <div className='search-icon-container'>
             <Search className='search-icon' size={18} />
-
           </div>
         </div>
         <div className='mic-icon-container'>
           <Mic size={20} className='mic-icon' />
-          
         </div>
       </div>
 
       <div className='nav-right flex-div'>
+        {/* Example music and home icons */}
+        <Home size={22} className='nav-icon' title="Home" />
+        <Music size={22} className='nav-icon' title="Music" />
         <div className='create-button flex-div'>
           <Plus size={20} className='create-icon' />
           <span>Create</span>
         </div>
         <Bell size={22} className='nav-icon' />
-        <img src={profile_icon} className='user-icon' alt="Profile Icon" /> 
+        <img src={profile_icon} className='user-icon' alt="Profile Icon" />
       </div>
     </nav>
   );
